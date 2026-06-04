70c388170f5c840680b083c728d0dd2a167b2035
Sane Sunil <sanesunil.falseofficial@gmail.com>
2026-06-04 16:12:26 +0530

    refactor: resume is fetched from the portfolio.json for gui

--- src/gui/apps/resume.js
+++ src/gui/apps/resume.js
@@ -1,10 +1,13 @@
-export function renderResume() {
+export function renderResume(data) {
+  const url = data.resumeUrl;
+  const enc = url.replace(/ /g, '%20');
+  const filename = url.split('/').pop();
   return `
     <div class="rs-layout">
       <div class="rs-bar">
-        <a class="rs-dl" href="assets/Sane%20Sunil.pdf" download="Sane Sunil.pdf">⬇ Download</a>
+        <a class="rs-dl" href="${enc}" download="${filename}">⬇ Download</a>
       </div>
-      <iframe class="rs-frame" src="assets/Sane%20Sunil.pdf#toolbar=0" title="Resume"></iframe>
+      <iframe class="rs-frame" src="${enc}#toolbar=0" title="Resume"></iframe>
     </div>
   `;
 }
